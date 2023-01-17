57386e2f02fe41f9b484dbde6de52ab8a0a14347
Victor Ajagunna <victor-ajagunna@hotmail.com>
2023-01-17 16:31:01 +0100

    cloudinary commit

--- Image-upload-cloudinary/server/index.js
+++ Image-upload-cloudinary/server/index.js
@@ -2,14 +2,51 @@ require('dotenv').config();
 
 const express = require("express");
 const app = express();
-const PORT = 8600;
+const PORT = process.env.PORT || 8600;
+const cors = require("cors");
+const cloudinary = require("./cloudinary/cloudinary");
+const { response } = require('express');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
+app.use(express.json({ limit: '50mb'}));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.get("/", (req, res) => { 
     res.send('Welcome to iBukun cloudinary project');
 });
+
+app.post("/", async (req, res) => {
+  const { images } = req.body;
+  const uploadedImgs = images.map(async (image) => {
+    const upload = await cloudinary.uploader.upload(
+      image,
+      {
+        upload_preset: "unsigned_upload",
+        allowed_formats: ["png", "jpg", "jpeg", "svg", "ico", "gif", "webp"],
+      },
+      function (error, result) {
+        if (error) {
+          console.log(error);
+        }
+      }
+    );
+    return upload;
+  });
+
+  try {
+    const fulfilled = await Promise.all(uploadedImgs).then((values) => {
+      return values;
+    });
+    const publicIds = fulfilled.map((image) => {
+      return image.public_id;
+    });
+    console.log(publicIds);
+    res.status(200).json(publicIds);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
